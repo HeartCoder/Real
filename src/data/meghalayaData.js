b52328eac473e943289b9125b7abc6886452b982
Pankaj Doley <pankajdoley91@gmail.com>
2025-05-23 03:26:23 +0530

    Update meghalayaData.js

--- src/data/meghalayaData.js
+++ src/data/meghalayaData.js
@@ -1,15 +1,15 @@
-
 import { meghalayaBaseDestinations } from './meghalayaBaseData';
 import { meghalayaKazirangaExplorerPackage } from './meghalayaExplorerPackage';
 import { getNewPriceDetails } from '@/lib/priceUtils';
 
+// Helper to attach new price fields to each destination
 const applyNewPrices = (destinations) => {
   return destinations.map(dest => {
     const priceDetails = getNewPriceDetails(dest.duration, dest.id, dest.priceData);
     return {
       ...dest,
-      priceDisplay: priceDetails.discounted, 
-      originalPriceDisplay: priceDetails.original !== priceDetails.discounted ? priceDetails.original : null
+      priceDisplay: priceDetails.discounted,
+      originalPriceDisplay: priceDetails.original
     };
   });
 };
